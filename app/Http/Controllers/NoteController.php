36aaadaf7767cfd903029e96347b13830dc6ac63
Jon <jzabalabur@fpzornotza.com>
2024-12-16 13:30:31 +0100

    Notes insert

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -66,6 +66,8 @@ class NoteController extends Controller
      */
     public function destroy(Note $note)
     {
-        //
+        $note=Note::where('id',$id)->delete();
+        return redirect()->route('note.index');
+
     }
 }
--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -16,7 +16,13 @@
                     <strong><h3>{{$note->id}} - {{$note->title}}</h3></strong><p>{{$note->created_at}}</p>
                     <p>{{$note->description}}</p>
                     <button type="button" class="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Update</button>
-                    <button type="button" class="mt-4 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
+                        <form action="{{ route('note.destroy', ['note' => $note->id]) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="mt-4 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
+                    </form>
+                   
+                   
                 </div>
             </div>
         </div>
